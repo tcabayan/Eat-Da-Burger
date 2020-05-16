8e40a0fa882cac83a92605aeb8aabeaf556c1408
Tamar Cabayan <tcabayan@gmail.com>
2020-05-15 20:22:27 -0700

    still trying to get server to work

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const methodOverride = require('method-override')
 const PORT = process.env.PORT || 3000;
 const router = require('./controllers/burgers_controller')
 
-// Add Routes
 //Serve static content for the app from the "public" directory in the application directory
 //app.use(express.static(__dirname + '/public'));
 app.use(express.static(process.cwd()+ '/public'))
@@ -16,19 +15,15 @@ app.use(express.static(process.cwd()+ '/public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
 // Set up handlebars view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+// Import routes and give the server access to them
 app.use('/', router);
 
 // Connect to the database and start express server
-connection.connect(err => {
-    if (err) {
-        console.log(err);
-    } else {
-        app.listen(PORT, () => {
-            console.log(`App listening on port ${PORT}`);
-        });
-    }
-});
+app.listen(port);
