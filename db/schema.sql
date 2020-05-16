ba148591819efd08c8aaf9cbf61063de654bbaa9
Tamar Cabayan <tcabayan@gmail.com>
2020-05-15 21:07:48 -0700

    issues with table

--- db/schema.sql
+++ db/schema.sql
@@ -3,8 +3,8 @@ CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE BURGERS (
-  id          INT AUTO_INCREMENT,
+  id INT AUTO_INCREMENT,
   burger_name VARCHAR(100) NOT NULL,
-  devoured    BOOLEAN      NOT NULL
-  PRIMARY KEY(id)
+  devoured BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
 );
