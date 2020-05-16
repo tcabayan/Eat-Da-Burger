3c489b2109440dd6316df535902e98ce85715ff5
Tamar Cabayan <tcabayan@gmail.com>
2020-05-15 20:12:09 -0700

    Updated server.js

--- server.js
+++ server.js
@@ -3,19 +3,24 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const exphbs = require("express-handlebars");
 const app = express();
+const methodOverride = require('method-override')
 const PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+const router = require('./controllers/burgers_controller')
 
 // Add Routes
-app.use(express.static(__dirname + '/public'));
-app.use('/', require('./controllers/burgerController'));
+//Serve static content for the app from the "public" directory in the application directory
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(process.cwd()+ '/public'))
+
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Set up handlebars view engine
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+app.use('/', router);
 
 // Connect to the database and start express server
 connection.connect(err => {
